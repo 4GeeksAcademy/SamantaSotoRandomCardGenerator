fc8b5dba88f506185eaf90d8ac2c23d4ac7184ed
Leticia Soto <samantasoto@gmail.com>
2024-09-24 01:42:12 +0000

    Generador de cartas casi listo
    Genera cartas aleatorias cada 10 segundos
    Genera cartas nuevas al hacer click en boton New card

--- src/app.js
+++ src/app.js
@@ -7,5 +7,46 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function generateCard() {
+    let value = [
+      "A",
+      "K",
+      "Q",
+      "J",
+      "10",
+      "9",
+      "8",
+      "7",
+      "6",
+      "5",
+      "4",
+      "3",
+      "2"
+    ];
+    let suitCard = ["♦", "♥", "♠ ", "♣"];
+    let randomValue = Math.floor(Math.random() * value.length);
+    let randomSuitCard = Math.floor(Math.random() * suitCard.length);
+
+    if (suitCard[randomSuitCard] == "♥" || suitCard[randomSuitCard] == "♦") {
+      suiteTop.style.color = "red";
+      suiteBottom.style.color = "red";
+    } else {
+      suiteTop.style.color = "black";
+      suiteBottom.style.color = "black";
+    }
+
+    document.getElementById("value").innerHTML = value[randomValue];
+    document.getElementById("suiteTop").innerHTML = suitCard[randomSuitCard];
+    document.getElementById("suiteBottom").innerHTML = suitCard[randomSuitCard];
+  }
+
+  generateCard();
+
+  setInterval(generateCard, 10000);
+
+  let refresh = document.getElementById("refresh");
+  refresh.addEventListener("click", _ => {
+    //location.reload();
+    generateCard();
+  });
 };
